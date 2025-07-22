e54e7db29fa73a10a28226e539f9ab215f2785c4
hemali.das <hemali.das@tpnodl.com>
2025-07-22 16:04:39 +0530

    sign up code updated

--- src/app/components/signup/signup.ts
+++ src/app/components/signup/signup.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule, RouterModule, CommonModule],
   templateUrl: './signup.html',
-  styleUrl: './signup.css'
+  styleUrl: './signup.css',
 })
 export class Signup {
+  name = '';
+  email = '';
+  password = '';
 
+  constructor(private auth: Auth, private router: Router) {}
+
+  signup() {
+    if (!this.name || !this.email || !this.password) {
+      alert('All fields are required.');
+      return;
+    }
+    this.auth.signup({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    });
+    alert('Signup successful! Please login.');
+    this.router.navigate(['/auth/login']);
+  }
 }
